3025f28b8578bf590ed36615d621168e05d9cae4
Charles Palmenta <palmentac@gmail.com>
2020-05-07 23:07:25 -0400

    2 events done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const mouseNav = document.querySelectorAll('a');
+mouseNav.forEach(link => {
+    link.addEventListener('mouseover', (event) =>{
+        event.target.style.color = '#17A2B8';
+    })
+})
+mouseNav.forEach(link => {
+    link.addEventListener('mouseleave', (event) =>{
+        event.target.style.color = '#212529';
+    })
+})
